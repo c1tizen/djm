0e0115de9d0570af06a322f29f25f8ce5af21e20
c1tizen <timotejferenc@gmail.com>
2021-04-01 19:31:11 +0200

    Fix build bugs and delete PieData

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import MainData from "./components/MainData"
 import './App.css';
-import PieData from "./components/PieData"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Aktuálne výsledky* z <a target="_blank" href="https://www.profesia.sk/">profesia.sk</a></h2>
+        <h2>Aktuálne výsledky* z <a target="_blank" rel="noreferrer" href="https://www.profesia.sk/">profesia.sk</a></h2>
         <p className="vysvetlivka">*Dáta sú zobrazené podľa počtu výsledkov</p>
       </header>
       <main>
